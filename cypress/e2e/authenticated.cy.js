6ce932092a244ecf238736d016e320aeb32961d4
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-03-04 13:33:34 -0300

    add ci

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -34,7 +34,7 @@ describe('Scenarios where authentication is a pre-condition', () => {
       .its('state')
       .should('be.equal', 'Complete')
   })
-  it.only('logs out', () => {
+  it.skip('logs out', () => {
     cy.visit('/')
     cy.wait('@getNotes')
     if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
